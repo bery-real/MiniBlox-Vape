97f63c69b279a14a1d60844794d92e7439e92bae
bery <uraniusrevive@gmail.com>
2025-01-02 16:30:14 -0800

    Update injection.js

--- injection.js
+++ injection.js
@@ -1,1 +1,1 @@
-var _0x9z7v743vyz815cf84c=_0x841ueuv4838u432v38q4;var _0x841ueuv4838u432v38q4=function(){console["log"]("in development")};_0x9z7v743vyz815cf84c();
+var _0x841ueuv4838u432v38q4=function(){console["log"]("vape for miniblox progress done: 0%")};_0x841ueuv4838u432v38q4();
